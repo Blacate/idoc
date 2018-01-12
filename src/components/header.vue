63e80fc412086a150003441cf98e778abe8c5503
Blacate <blacated@gmail.com>
2018-01-12 17:16:33 +0800

    init project

---
+++ src/components/header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="header">
+      <span class="logo">iDocument</span>
+      <nav>
+          <ul class="menu">
+              <li>首页</li>
+              <li>公共课</li>
+              <li>专业课</li>
+          </ul>
+      </nav>
+      <div class="search"></div>
+  </header>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="sass">
+    .header {
+        height: 100px;
+        width: 1280px;
+        margin: 0 auto;
+        background: #fff;
+        .log {
+            
+        }
+        nav {
+            dispaly: inline-block;
+            li {
+                
+            }
+        }
+    }
+</style>
+
